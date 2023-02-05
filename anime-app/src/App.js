bf37d01f5edb30753ddc3ec84ae5f19b9a616ca4
Amado Azua III <amado.azua3@gmail.com>
2023-02-05 16:07:33 -0600

    Added node.js project folder, and other features

--- anime-app/src/App.js
+++ anime-app/src/App.js
@@ -1,28 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import {XRapidAPIKey, XRapidAPIHost} from './keys.js';
-import axios from 'axios';
-import './DatabaseAnimeDB.js';
 
-
-const options = {
-  method: 'GET',
-  url: 'https://anime-db.p.rapidapi.com/anime',
-  params: {page: '1', size: '10000', sortBy: 'ranking', sortOrder: 'asc'},
-  headers: {
-    'X-RapidAPI-Key': XRapidAPIKey,
-    'X-RapidAPI-Host': XRapidAPIHost
-  }
-};
-
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
 
 function App() {
-
 
   return (
     <div className="App">
