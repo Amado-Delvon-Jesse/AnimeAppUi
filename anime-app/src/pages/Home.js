071e74bddce2f8d7b5acd33c1bfd5f353fd69399
Amado Azua III <amado.azua3@gmail.com>
2023-02-21 14:56:05 -0600

    Top 5 anime only show on home

--- anime-app/src/pages/Home.js
+++ anime-app/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import AnimeCard from "../AnimeCard";
+
 
 const Home = () => {
 
     return(
         <div className="App">
             <h1>Home</h1>
+            <AnimeCard />
         </div>
 
     )
